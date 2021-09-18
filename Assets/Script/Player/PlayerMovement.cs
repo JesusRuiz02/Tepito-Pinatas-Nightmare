27cb984f594fe20ca616335e6a15d0367140d4d8
Justpeli <zap407@amerike.edu.mx>
2021-09-17 23:05:45 -0500

    Animations

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator AC;
+    public SpriteRenderer spriteRenderer;
     public float moveSpeed = 5f;
-
+    bool compro = false;
+    
     public Rigidbody2D rb;
     Vector2 movement;
 
@@ -13,7 +16,46 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.x>0)
+        {
+            compro = false;
+            AC.SetBool("M_Left", true);
+            spriteRenderer.flipX = true;
+            AC.SetBool("M_Down", false);
+            AC.SetBool("M_Up", false);
+        }
+        else if(movement.x<0)
+        {
+            compro = false;
+            AC.SetBool("M_Left", true);
+            spriteRenderer.flipX = false;
+            AC.SetBool("M_Down", false);
+            AC.SetBool("M_Up", false);
+        }
+        else
+        {
+            compro = true;
+        }
+        if(compro==true)
+            {
+            if (movement.y > 0)
+            {
+                AC.SetBool("M_Left", false);
+                AC.SetBool("M_Up", true);
+                AC.SetBool("M_Down", false);
+            }
+            else
+            {
+                AC.SetBool("M_Left", false);
+                AC.SetBool("M_Up", false);
+                AC.SetBool("M_Down", true);
+            }
+        }
+        
     }
+    //Ver orientacion para ataque
+
+
 
     void FixedUpdate() //Movement
     {
