19ed6b25f302ac2dfbb40e96b5bfec5b69ff5c3d
JesusRuiz02 <zap295@amerike.edu.mx>
2021-09-18 19:46:22 -0600

    GeneradorSpawn

--- Assets/Script/Enemy/EnemyAI.cs
+++ Assets/Script/Enemy/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,7 +20,12 @@ public class EnemyAI : MonoBehaviour
 
     Seeker seeker;
     Rigidbody2D rb;
-     
+
+    private void Awake()
+    {
+        this.transform.position= new Vector3(transform.position.x,transform.position.y, 0);
+    }
+
     void Start()
     {
         seeker = GetComponent<Seeker>();
